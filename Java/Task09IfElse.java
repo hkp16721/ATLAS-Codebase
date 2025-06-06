e9e5f5efdffd9b7f0cdd2c1f8f1e7d042319ce51
Hemant <hkkumar16@gmail.com>
2025-06-06 15:59:55 +0530

    Arrays and others

--- Java/Task09IfElse.java
+++ Java/Task09IfElse.java
@@ -1,13 +1,15 @@
 public class Task09IfElse{
 public static void main(String[] args){
+
+System.out.println(args[0]);
 int a = Integer.parseInt(args[0]);
 int b = Integer.parseInt(args[1]);
 
-if(a>b){
-System.out.println("a>b");
-}else{
-System.out.println("b>a");
-}
+//if(a>b){
+//System.out.println("a>b");
+//}else{
+//System.out.println("b>a");
+//}
 
 }
 }
---
+++ Java/Task14Arrays.java
@@ -0,0 +1,10 @@
+public class Task14Arrays {
+    public static void main(String[] args){
+        int[] list1={1,2,3,4,5};
+        System.out.println(list1[1]);
+        char[] list2={'a','b','c','d'};
+        System.out.println(list2[3]);
+        System.out.println(list2.length);
+//        System.out.println(list1[7]);
+    }
+}
