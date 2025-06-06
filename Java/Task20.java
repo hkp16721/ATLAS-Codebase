b1754a4273ee657410afe59f8b294e3c13084377
Hemant <hkkumar16@gmail.com>
2025-06-06 17:35:46 +0530

    Arrays

---
+++ Java/Task20.java
@@ -0,0 +1,16 @@
+public class Task20 {
+    public static void main(String[] args){
+        char[] name={'H','e','m','a','n','t'};
+        System.out.println(name);
+        int n =name.length;
+        System.out.printf("Total %d letters in my name \n",n);
+//        for(int i=0;i<n;i++){
+//            System.out.println(name[i]);
+//        }
+        for (char c : name) {
+            System.out.println(c);
+        }
+
+    }
+}
+
