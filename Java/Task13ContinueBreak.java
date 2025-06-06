f7de720c1a96676d8b9105f3c9fa1d69419589a7
Hemant <hkkumar16@gmail.com>
2025-06-06 15:36:49 +0530

    Task 13 break and continue

---
+++ Java/Task13ContinueBreak.java
@@ -0,0 +1,11 @@
+public class Task13ContinueBreak {
+    public static void main(String[] args){
+        for(int i=10;i>=1;i--){
+            if(i==7 || i==5) {
+                continue;
+            }
+            System.out.println(i);
+
+        }
+    }
+}
