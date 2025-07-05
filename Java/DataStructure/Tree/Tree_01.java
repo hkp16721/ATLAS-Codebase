69d1a22dc59bd842c342026b32b56a4e02386fca
Hemant <hkkumar16@gmail.com>
2025-07-05 15:51:05 +0530

    BST Task till  04

--- Java/DataStructure/Tree/Tree_01.java
+++ Java/DataStructure/Tree/Tree_01.java
@@ -54,14 +54,14 @@ public class Tree_01 {
     public static void main(String[] args) {
         tree bst = new tree();
 
-        bst.insert(20);
+        bst.insert(50);
         bst.insert(10);
-        bst.insert(30);
+        bst.insert(400);
+        bst.insert(70);
         bst.insert(5);
-        bst.insert(15);
-        bst.insert(25);
-        bst.insert(35);
-        bst.insert(12);
+//        bst.insert(25);
+//        bst.insert(35);
+//        bst.insert(12);
 
         bst.inOrderTraversal();
     }
