3aefb9996d80aa3107bb2990aa9a075cecfe1d76
Hemant <hkkumar16@gmail.com>
2025-08-01 11:02:37 +0530

    task 1-4

---
+++ Java/DataStructure/CodingTasks/student.java
@@ -0,0 +1,22 @@
+package DataStructure.CodingTasks;
+
+public class student {
+    private String name;
+    private String address;
+    private int phone;
+    private int roll;
+
+    public static void registrationDetails(String name, String address, int phone){
+
+    }
+    public static int feesCalc(){
+        return 1;
+    }
+    public static int marksCalc(){
+        return 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
